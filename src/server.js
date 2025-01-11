6b28cc127370d710200f6e0d3fd286ab9d4fec66
DANNURA <nurudeendanjuma93@mail.com>
2025-01-11 21:44:53 +0100

    updates

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5020;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
